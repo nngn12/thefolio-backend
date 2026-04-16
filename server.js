322e49ada8a09c31623d06690c34f8cc34372533
nngn12 <analeahbasco1@gmail.com>
2026-04-16 17:56:25 +0800

    final cors add

--- server.js
+++ server.js
@@ -34,8 +34,8 @@ app.use(express.json());
 
 app.use(cors({
   origin: [
-    'http://localhost:3000',
-    'https://thefolio-frontend.vercel.app',
+    "http://localhost:3000",
+    "https://thefolio-frontend-zeta.vercel.app",
   ],
   credentials: true
 }));
